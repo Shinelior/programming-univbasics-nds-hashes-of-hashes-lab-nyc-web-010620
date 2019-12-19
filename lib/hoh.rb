99c6f9d8ed8163d68fd5d0aa635c450abc08464a
lior Shine <shinelior@gmail.com>
2019-12-19 18:45:04 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -9,7 +9,7 @@ Hash = {
    sub_category: {
      label: "Phylum",
      sub_category: {
-      label: "Class",sub_category: {label: "Order",sub_category{ label: "Family",sub_category: {label: "Genus",sub_category{ label: "Species",
+      label: "Class",sub_category: {label: "Order",sub_category{ label: "Family",sub_category: {label: "Genus",sub_category{ label: "Species",  sub_category: nil}}}}
      }
    }
  }
@@ -27,7 +27,7 @@ Hash = {
 # 
 # PORTION_4 = {
 #  label: "Species",
-   sub_category: nil
+  # sub_category: nil
 # }
 
 def naming_system
